077c52e5c0230167202fede2c10d47cbbb063808
Alexggp <alex.ggperez@gmail.com>
2015-05-26 12:12:05 +0200

    funcion para rellenar el ranking "fill_ranking" con json ficticio

--- js/pregame.js
+++ js/pregame.js
@@ -1,12 +1,51 @@
 $(document).ready(function() {
 
-	$( "#Gamefront #play" ).click(function() {
+	$( "#Gamefront #play img" ).click(function() {
         $( "#Gamefront" ).hide();
         $( "#container" ).show();
         Game.initialize("game",sprites,startGame);
 	});
+    
+    
+    // datos simulados, deberian llegar como json con un get
+    var data ={
+        'ranking':[
+            {'name':'Joaquin Armando','time':'256'},
+            {'name':'pepito perez','time':'367'},
+            {'name':'Custodio Trujillo','time':'125'},
+            {'name':'Roldan Cabrera','time':'326'},
+            {'name':'Fulgencio Chavarría','time':'158'},
+            {'name':'Rico Capello','time':'265'},
+            {'name':'Francisco Robles','time':'157'},
+            {'name':'Leandro De Leon','time':'269'},
+            {'name':'Anacleto Noguerra','time':'197'},
+            {'name':'Regulo Ruiz','time':'105'},
+            {'name':'Adrian Gallo','time':'136'},
+            {'name':'Tito Ibarra','time':'187'},              
+        ],
+        'user': 'Rico Capello',
+        'best':{'position':'7','time':'311'}
+    }
 
+    var fill_ranking = function(data){
+        if (data.best.position) {
+            $('#Gamefront #yourBest').html(data.best.position);
+            $('#Gamefront #yourBestTime').html(data.best.time+"''");
+        }
 
+        var ranking=_.sortBy(data.ranking, function(obj){return -obj.time});
+        for (var i=0;i<5;i++){
+            $('#Gamefront #ranking ol').append("<li>"+ranking[i].name+"<span class='record'>"+ranking[i].time+"''</span></li>");
+        }
+        var even = _.find(ranking, function(obj){ return obj.name == data.user;});
+        if (even) {
+            $('#Gamefront #today').html(ranking.indexOf(even));
+            $('#Gamefront #todayTime').html(even.time+"''");
+        }
 
+        
+    }
 
+    fill_ranking(data);
+    
 });
